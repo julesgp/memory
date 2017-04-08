43ff01ba2b42bac6ea1b52247fd7d4aad5cf361b
Julian Garcia <jugar28@gmail.com>
2017-04-08 16:00:19 -0400

    added partial, and organized files

--- src/components/game.js
+++ src/components/game.js
@@ -4,6 +4,32 @@ export default class Game extends Component{
 	render(){
 		return(
 			<section>
+				<div className="cardsWrapper">
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+					<div className="card">
+					</div>
+				</div>
 				<div className="buttonsWrapper">
 					<button onClick={this.props.startGame}>Start</button>
 				</div>
